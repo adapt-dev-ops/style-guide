84ddc5685f967f59857e7b49677e47d04f03b46e
DESKTOP-KQ29CJP\user <yusmsou@adaptkorea.com>
2025-11-26 16:38:36 +0900

    공용코드 수정

--- assets/js/site-common.js
+++ assets/js/site-common.js
@@ -1,26 +1,4 @@
-/* ------------------------------------------------------
-* 01. &nbsp/공백 자동삽입 제거
-* ------------------------------------------------------ */
-(function($){
-    const SEL='img,input,br,hr,meta,link,source,track,col,area,base,wbr,embed,param';
-    const isSpace = n => n && n.nodeType===3 && /^[\s\u00A0]+$/.test(n.nodeValue||'');
-    const clean = root => $(root).find(SEL).each((_,el)=>{
-        for(let n=el.nextSibling; isSpace(n);){ const r=n; n=n.nextSibling; r.remove(); }
-    });
-    $(clean); $(window).on('load',()=>clean(document));
-    new MutationObserver(ms=>{
-        ms.forEach(m=>{
-        m.addedNodes?.forEach(n=>{
-            if(n.nodeType===1) clean(n);
-            else if(isSpace(n) && n.previousSibling?.nodeType===1 && $(n.previousSibling).is(SEL)) n.remove();
-        });
-        if(m.type==='characterData' && isSpace(m.target)){
-            const p=m.target.previousSibling;
-            if(p?.nodeType===1 && $(p).is(SEL)) m.target.remove();
-        }
-        });
-    }).observe(document.body,{childList:true,subtree:true,characterData:true});
-})(jQuery);
+
 
 /* -----------------------------------------------------
 * 2. IP 기반 노출 제어 
