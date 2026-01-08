2dc765326f15e1934d6a3cb57dcd4f822c81029c
adapt-dev-ops <adapt_dev@adaptkorea.com>
2026-01-08 15:21:03 +0900

    Update: 푸드, 오브제, 95, 8apm, 풀리, 에이페 by dawson

--- src/forwarding.js
+++ src/forwarding.js
@@ -1,38 +1,70 @@
-// src/forwarding.js
-// 포워딩 설정 관리
-// Slack 봇으로 자동 업데이트됨
-
 export default {
   settings: [
     {
       "brands": [
-        "95"
+        "푸드"
       ],
       "openDays": [
-        "일",
-        "금"
+        "월",
+        "화",
+        "수"
       ],
-      "landingUrl": "https://95problems.com/event/bestsale26.html"
+      "landingUrl": "https://food-ology.co.kr/test.html"
     },
     {
       "brands": [
         "오브제"
       ],
       "openDays": [
-        "일",
-        "금"
+        "월",
+        "화",
+        "수"
       ],
-      "landingUrl": "https://obge.co.kr/event/bestsale26.html"
+      "landingUrl": "https://obge.co.kr/test.html"
     },
     {
       "brands": [
-        "푸드"
+        "95"
       ],
       "openDays": [
-        "일",
-        "금"
+        "월",
+        "화",
+        "수"
       ],
-      "landingUrl": "https://food-ology.co.kr/event/bestsale26.html"
+      "landingUrl": "https://95problems.com/test.html"
+    },
+    {
+      "brands": [
+        "8apm"
+      ],
+      "openDays": [
+        "월",
+        "화",
+        "수"
+      ],
+      "landingUrl": "https://8apm.co.kr/test.html"
+    },
+    {
+      "brands": [
+        "풀리"
+      ],
+      "openDays": [
+        "월",
+        "화",
+        "수"
+      ],
+      "landingUrl": "https://full-y.co.kr/test.html"
+    },
+    {
+      "brands": [
+        "에이페"
+      ],
+      "openDays": [
+        "월",
+        "화",
+        "수"
+      ],
+      "landingUrl": "https://epais.kr/test.html"
     }
   ]
 };
