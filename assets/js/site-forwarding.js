ab49cf60bb96375b229c39952ef3598b5de1ff4e
김도근 <dawson@adaptkorea.com>
2026-02-27 15:42:30 +0900

    update

--- assets/js/site-forwarding.js
+++ assets/js/site-forwarding.js
@@ -228,12 +228,7 @@
       "brands": [
         "에이페"
       ],
-      "openDays": [
-        "수",
-        "목",
-        "토",
-        "일"
-      ],
+      "openDays": [],
       "landingUrl": "https://epais.kr/product/detail.html?product_no=83",
       "targetPath": "/product/detail.html?product_no=103"
     }
