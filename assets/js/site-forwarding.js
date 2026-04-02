dd100fd94f3c6ce27d05c9beb8bd3c9a8a96a01f
gksmftor956 <gksmftor956@naver.com>
2026-04-02 14:12:00 +0900

    에이페 포워딩 변경

--- assets/js/site-forwarding.js
+++ assets/js/site-forwarding.js
@@ -280,6 +280,21 @@
       "brands": [
         "에이페"
       ],
+      "openDays": [
+        "수",
+        "목",
+        "토",
+        "일"
+      ],
+      "landingUrl": "https://epais.kr/product/detail.html?product_no=83",
+      "targetPath": "/product/detail.html?product_no=98",
+      "reservedDay": "",
+      "removeDay": ""
+    },
+    {
+      "brands": [
+        "에이페"
+      ],
       "openDays": [],
       "landingUrl": "https://epais.kr/product/detail.html?product_no=83",
       "targetPath": "/product/detail.html?product_no=103",
