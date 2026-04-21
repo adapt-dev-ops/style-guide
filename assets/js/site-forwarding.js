a8ddf28567951457667f813e94c180103792d13b
김도근 <dawson@adaptkorea.com>
2026-04-21 17:10:11 +0900

    update

--- assets/js/site-forwarding.js
+++ assets/js/site-forwarding.js
@@ -108,7 +108,12 @@
       "brands": [
         "8apm"
       ],
-      "openDays": [],
+      "openDays": [
+        "수",
+        "목",
+        "토",
+        "일"
+      ],
       "landingUrl": "https://8apm.co.kr/product/list.html?cate_no=24",
       "targetPath": "/event/friendsale26.html"
     },
@@ -116,12 +121,7 @@
       "brands": [
         "8apm"
       ],
-      "openDays": [
-        "수",
-        "목",
-        "토",
-        "일"
-      ],
+      "openDays": [],
       "landingUrl": "https://8apm.co.kr/product/list.html?cate_no=24",
       "targetPath": "/product/list.html?cate_no=24&page_type=friendsale26"
     },
