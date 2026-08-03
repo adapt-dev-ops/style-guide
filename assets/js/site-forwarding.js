8fc129fe2888ad06aa3957e265b39ae44ca43248
adapt-jacob <gksmftor956@naver.com>
2026-08-03 16:11:20 +0900

    에이페 포워딩 추가

--- assets/js/site-forwarding.js
+++ assets/js/site-forwarding.js
@@ -326,6 +326,16 @@
       "reservedDay": "",
       "removeDay": ""      
     },
+    {
+      "brands": [
+        "에이페"
+      ],
+      "openDays": [],
+      "landingUrl": "https://epais.kr/product/detail.html?product_no=83",
+      "targetPath": "/product/detail.html?product_no=98",
+      "reservedDay": "",
+      "removeDay": ""      
+    },
   ];
 
 
